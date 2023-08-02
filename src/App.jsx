0e5f3ab623b062b53d91c4c5cf467627c3ecbbdc
isaacbaldridge <baldridgela@alumni.vcu>
2023-08-02 11:33:16 -0400

    created empty components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
-
+import {Route, Routes} from "react-router-dom"
+import Profile from "./components/Profile"
 import './App.css'
 
 function App() {
 
   return (
     <div>
+      <Routes>
+        <Route path="/profile" element={<Profile />}></Route>
+
+      </Routes>
 
     </div>
   )
