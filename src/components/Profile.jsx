bc93b3d198d89ff4655395a202e5c3196528adcf
isaacbaldridge <baldridgela@alumni.vcu>
2023-08-06 19:52:54 -0400

    styled profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,31 +4,34 @@ const Profile = ({token, user}) => {
 
 if (token && user.posts) {
     const myPosts = user.posts
-    console.log(myPosts)
     const myActivePosts = myPosts.filter((post) => post.active)
-    console.log(myActivePosts)
+
     const myActivePostsMap = myActivePosts.map((post, index) => 
-    <div key={index}>
+    <div className="activePosts"  key={index}>
         <h2>{post.title} | {post.price}</h2>
         <h3>{post.location}</h3>
         <p>{post.description}</p>
     <div>
+        <h2>Messages:</h2>
+        <hr/>
         {post.messages.map((msg, index) =>
         <div key={index}>
             <h3>From: {msg.fromUser.username}</h3>
             <p>{msg.content}</p>
+            <hr/>
         </div>)}
     </div>
 </div>)
 
 const myMessagesMap = myActivePosts.map((post, index) => 
-<div key={index}>
+<div className="activePosts" key={index}>
     <div>
         {post.messages.map((msg, index) => 
         <div key={index}>
             <h3>From: {msg.fromUser.username}</h3>
             <p>{msg.content}</p>
             <Link to={`/posts/${post._id}`}>VIEW MY POST: {post.title}</Link>
+            <hr/>
         </div>)}
     </div>
 </div>)
